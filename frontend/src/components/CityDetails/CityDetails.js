c9815c7f5865d63770e7628bbcb6d27368bd97b5
maimunafaria <bsse1222@iit.du.ac.bd>
2023-11-04 08:37:48 +0600

    Fetched City Info

--- frontend/src/components/CityDetails/CityDetails.js
+++ frontend/src/components/CityDetails/CityDetails.js
@@ -1,66 +1,59 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { Card, Typography, Divider } from 'antd';
 import { Pie } from 'react-chartjs-2';
 import 'chart.js/auto';
-import  { useState, useEffect } from 'react';
-import axios from 'axios'; 
 import { useParams } from 'react-router-dom';
+
 const CityDetails = () => {
   const { cityName } = useParams();
   const [cityInfo, setCityInfo] = useState(null);
+
   useEffect(() => {
     axios
       .get(`http://localhost:3000/cty/city/${cityName}`)
       .then((response) => {
-        console.log("hi")
-        setCityInfo(response.data);
-        console.log(response.data)
+        const cityData = response.data[0]
+        setCityInfo(cityData);
+        console.log(cityData)
       })
       .catch((error) => {
         console.error(error);
       });
-  }, [cityName]);
-  // Dummy data for the pie chart
-  // const pieChartData = {
-  //   labels: ['N2O', 'O2', 'CO2'],
-  //   datasets: [
-  //     {
-  //       data: [20, 30, 50], // Dummy values for N2O, O2, CO2
-  //       backgroundColor: ['#D0312D', 'rgb(255, 250, 0)', '#3377FF'], // Colors for the segments
-  //     },
-  //   ],
-  // };
+    
+  }, []);
+
+  // Check if the cityInfo is available
+  if (!cityInfo) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr', gap: '16px' }}>
-      {/* <div>
+      <div>
         <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}>
-          <Card title={cityData.name}>
-            <p><strong>Country Name:</strong> {cityData.countryName}</p>
-            <p><strong>GDP:</strong> {cityData.gdp}</p>
-            <p><strong>GDP per Capita:</strong> {cityData.gdpPerCapita}</p>
-            <p><strong>GDP Growth:</strong> {cityData.gdpGrowth}</p>
-            <p><strong>Total Population:</strong> {cityData.totalPopulation}</p>
-            <p><strong>Population Growth:</strong> {cityData.populationGrowth}</p>
+          <Card title={cityInfo.city_name}>
+            <p><strong>Country Name:</strong> {cityInfo.country_name}</p>
+            <p><strong>GDP:</strong> {cityInfo.gdp}</p>
+            <p><strong>GDP per Capita:</strong> {cityInfo.gdp_per_capita}</p>
+            <p><strong>GDP Growth:</strong> {cityInfo.gdp_growth}</p>
+            <p><strong>Total Population:</strong> {cityInfo.population}</p>
+            <p><strong>Population Growth:</strong> {cityInfo.population_growth}</p>
             <p><strong>Air Quality Metrics:</strong></p>
             <ul>
-              <li><strong>AQ:</strong> {cityData.AQ}</li>
-              <li><strong>PM:</strong> {cityData.PM}</li>
-              <li><strong>PM10:</strong> {cityData.PM10}</li>
-              <li><strong>Ozone Levels:</strong> {cityData.ozoneLevels}</li>
+              <li><strong>AQ:</strong> {cityInfo.AQ}</li>
+              <li><strong>PM:</strong> {cityInfo.PM}</li>
+              <li><strong>PM10:</strong> {cityInfo.PM10}</li>
+              <li><strong>Ozone Levels:</strong> {cityInfo.ozoneLevels}</li>
             </ul>
             <Divider />
           </Card>
         </div>
       </div>
-      <div>
-        <div style={{ maxWidth: '400px', maxHeight: '400px' }}>
-          <Typography.Title level={5}>N2O, O2, CO2 Levels</Typography.Title>
-          <Pie data={pieChartData} />
-        </div>
-      </div> */}
     </div>
+  
   );
 };
 
 export default CityDetails;
+
